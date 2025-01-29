1846771dbbc8e4a0678e8e0fbc098ce46cee411d
Lyall <lyall@stamper.net>
2025-01-29 01:34:46 +0000

    Version bump.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -13,7 +13,7 @@ HMODULE thisModule;
 
 // Fix details
 std::string sFixName = "FF7RebirthFix";
-std::string sFixVersion = "0.0.4";
+std::string sFixVersion = "0.0.5";
 std::filesystem::path sFixPath;
 
 // Ini
